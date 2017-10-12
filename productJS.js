c7cea89618608783d0d1c16f5b06261b77c241dd
Faizaan Chishtie <fchishtie@Faizaans-MacBook-Pro.local>
2017-10-12 10:57:49 -0400

    added external javascript file for product page, liked from html page.

---
+++ productJS.js
@@ -0,0 +1,9 @@
+/*
+@Author Faizaan Chishtie
+Contains functions for product page on NutriDaili website
+*/
+
+function JsFunction(){
+  var text=document.getElementsById('text_in').value;
+  document.getElementsById("output").innerHTML = JsFunction();
+}
