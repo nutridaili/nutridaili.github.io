a94cd188175c5a223d0f9f21dcc010c55148b78f
Faizaan Chishtie <fchishtie@Faizaans-MacBook-Pro.local>
2017-10-16 12:48:21 -0400

    Starting to build calorie counter

--- productJS.js
+++ productJS.js
@@ -5,5 +5,5 @@ Contains functions for product page on NutriDaili website
 
 function JsFunction(){
   var text=document.getElementsById('text_in').value;
-  document.getElementsById("output").innerHTML = JsFunction();
+  document.getElementsById("output").innerHTML = text;
 }
