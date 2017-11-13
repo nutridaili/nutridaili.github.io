0242c52724996ce297231cdc73eb8807521df5fe
Your Name <you@example.com>
2017-11-12 23:09:40 -0500

    revamp

--- productJS.js
+++ productJS.js
@@ -4,31 +4,29 @@ Contains functions for product page on NutriDaili website
 */
 
 function CollectData(){
-  try {
-  weight = double(document.getElementsById('weight').value);
-  height = double(document.getElementsById('height').value);
-  age = double(document.getElementsById('age').value);
-  gender = document.getElementsById('gender').value;
-  exercise = double(document.getElementsByID('activity').value);
-  }
- catch {
-   alert("One or more of the fields contains an incorrect value!");
- }
-  document.getElementsById("output").innerHTML = calculateCalories(weight,height,age,gender,exercise);
-}
+    document.getElementById("output").innerHTML = "hi";
+    weight = document.getElementById('weight').value;
+    height = document.getElementById('height').value;
+    age = document.getElementById('age').value;
+    gender = document.getElementById('gender').value;
+    exercise = document.getElementById('activity').value;
+    s = "" + weight + ", " + height + ", " + age + ", " + gender + ", " + exercise + ".";
+    out = "You should be eating: " + calculateCalories(weight,height,age,gender,exercise) + " Calories a day!";
+    document.getElementById("output").innerHTML =  out;
+  };
 
-function calculateCalories(weight,height,age,gender,exercise){
-  alert(calculateBMR(weight,height,age,gender)*exercise)
-  return calculateBMR(weight,height,age,gender)*exercise;
-}
+  function calculateCalories(weight,height,age,gender,exercise){
+    return calculateBMR(weight,height,age,gender)*exercise;
+  };
 
-function calculateBMR(weight, height, age, gender){
-  if (gender=="F"){
-    //Women	BMR = (10 × weight in kg) + (6.25 × height in cm) - (5 × age in years) - 161
-    return 10*weight + 6.25*height - 5*age - 161;
-  }
-  if (gender=="M"){
-    //Men	BMR = (10 × weight in kg) + (6.25 × height in cm) - (5 × age in years) + 5
-    return 10*weight + 6.25*height - 5*age +5;
-  }
-}
+  function calculateBMR(weight, height, age, gender){
+    if (gender=="F"){
+      //Women	BMR = (10 × weight in kg) + (6.25 × height in cm) - (5 × age in years) - 161
+      return 10*weight + 6.25*height - 5*age - 161;
+    }
+    if (gender=="M"){
+      //Men	BMR = (10 × weight in kg) + (6.25 × height in cm) - (5 × age in years) + 5
+      return 10*weight + 6.25*height - 5*age +5;
+    }
+
+  };
