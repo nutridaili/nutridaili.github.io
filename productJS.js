aa7827d636806178d612b231d7480611020ada1f
Your Name <you@example.com>
2017-11-12 23:00:13 -0500

    Finished Calculator Javascript

--- productJS.js
+++ productJS.js
@@ -3,19 +3,32 @@
 Contains functions for product page on NutriDaili website
 */
 
-function JsFunction(){
-  var text=document.getElementsById('text_in').value;
-  document.getElementsById('output').innerHTML = text;
+function CollectData(){
+  try {
+  weight = double(document.getElementsById('weight').value);
+  height = double(document.getElementsById('height').value);
+  age = double(document.getElementsById('age').value);
+  gender = document.getElementsById('gender').value;
+  exercise = double(document.getElementsByID('activity').value);
+  }
+ catch {
+   alert("One or more of the fields contains an incorrect value!");
+ }
+  document.getElementsById("output").innerHTML = calculateCalories(weight,height,age,gender,exercise);
 }
 
-function calculateBMR(){
-
+function calculateCalories(weight,height,age,gender,exercise){
+  alert(calculateBMR(weight,height,age,gender)*exercise)
+  return calculateBMR(weight,height,age,gender)*exercise;
 }
 
-function out_feet(){
-
-}
-
-function out_cm(){
-
+function calculateBMR(weight, height, age, gender){
+  if (gender=="F"){
+    //Women	BMR = (10 × weight in kg) + (6.25 × height in cm) - (5 × age in years) - 161
+    return 10*weight + 6.25*height - 5*age - 161;
+  }
+  if (gender=="M"){
+    //Men	BMR = (10 × weight in kg) + (6.25 × height in cm) - (5 × age in years) + 5
+    return 10*weight + 6.25*height - 5*age +5;
+  }
 }
